fd4dca9f69b09a24ac5a83f15d826768ef6c5fe5
Jonatas Dourado <jonatasfontele@gmail.com>
2021-08-08 07:55:41 -0300

    Add files via upload

---
+++ notafiscal/Principal.java
@@ -0,0 +1,10 @@
+package notafiscal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Cliente cliente = new Cliente(100, "Lanchonete Salgado e Cia", "200.200.0002/20");
+		Produto milho = new Produto(110; "Milho"; 4.00f);
+	}
+
+}
