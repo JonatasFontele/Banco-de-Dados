10dc2d09779acc1f67adfba35007c0d57c9983fe
Jonatas Dourado <jonatasfontele@gmail.com>
2021-08-12 23:30:44 -0300

    Subconsulta correlacionada com cláusula WHERE
    Subconsulta correlacionada com cláusula WHERE x Consulta com DISTINCT e INNER JOIN

---
+++ SUBQUERY x DISTINCT INNER JOIN.sql
@@ -0,0 +1,16 @@
+-- Subconsulta correlacionada com cláusula WHERE
+SELECT   CUSTOMERID
+FROM     CUSTOMERS
+WHERE    (SELECT Count(*)
+          FROM   ORDERS
+          WHERE  CUSTOMERS.CUSTOMERID=ORDERS.CUSTOMERID)
+ORDER BY CUSTOMERS.CUSTOMERNAME
+
+-- Consulta com DISTINCT e INNER JOIN
+SELECT DISTINCT CUSTOMERS.CUSTOMERNAME
+FROM            CUSTOMERS
+INNER JOIN      ORDERS
+ON              CUSTOMERS.CUSTOMERID=ORDERS.CUSTOMERID
+ORDER BY        CUSTOMERS.CUSTOMERNAME;
+
+-- Ambas retornam o mesmo resultado e sem os valores nulos que o LEFT JOIN apresentaria, mas qual é a consulta mais rápida?
